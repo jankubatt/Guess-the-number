4a34b1d516d948d94727a5e9dcd2db2d29cabac2
Jan Kubát <jankubat.1@seznam.cz>
2019-06-14 18:42:46 +0200

    Přidejte soubory projektu.

---
+++ guessnum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace guessnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            Random random = new Random();
+            while (true)
+            {
+                Console.WriteLine("Guess number between 0-100");
+                int ranNum = random.Next(100);
+                int i = 0;
+
+                while (true)
+                {
+                    input = int.Parse(Console.ReadLine());
+                    
+                    if (input < ranNum)
+                    {
+                        Console.WriteLine("higher");
+                        i++;
+                    }
+                    else if (input > ranNum)
+                    {
+                        Console.WriteLine("Lower");
+                        i++;
+                    }
+                    else
+                    {
+                        Console.WriteLine(" ");
+                        Console.WriteLine("You won after {0} guesses, the number was {1}", i, ranNum);
+                        Console.WriteLine(" ");
+                        break;
+                    }
+                }
+            }
+        }      
+    }
+}
